f4eb9d9327350d1d3b0de6c74fb13a9700e3e315
Aaron Renner <aaron.renner@gmail.com>
2017-11-02 09:50:02 -0600

    Add credo, exdoc, dialyzer and travis

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule ExRiak.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "ExRiak",
+      source_url: "https://github.com/aaronrenner/ex_riak",
     ]
   end
 
@@ -21,8 +25,9 @@ defmodule ExRiak.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false},
     ]
   end
 end
